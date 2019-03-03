4810914e92cbf914d5426139be84bd4b1964f2c4
Mona Lisa <jb47472@hotmail.com>
2019-03-03 15:05:29 -0500

    issue with clearing div when pushing buttons

--- javascript/app.js
+++ javascript/app.js
@@ -16,6 +16,7 @@ function renderButtons() {
         // Adding the button to the HTML
         $("#giphy-view").append(a);
 
+
     }
 }
 
@@ -30,53 +31,96 @@ $("#add-giphy").on("click", function (event) {
     // The movie from the textbox is then added to our array
     sportsOptions.push(sportChoice);
 
+    //clears the value of the input box
+    $('#giphy-input').val("");
+
+
     // calling renderButtons which handles the processing of our movie array
     renderButtons();
-});
 
+
+
+});
 // Calling the renderButtons function at least once to display the initial list of movies
 renderButtons();
 
-$("button").on("click", function () {
-    // Grabbing and storing the data-animal property value from the button
-    var sports = $(this).attr("gif");
+function displayGifs() {
 
-    // Constructing a queryURL using the animal name
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        sports + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        // After data comes back from the request
-        .then(function (response) {
-            console.log(queryURL);
+    $("#giphy-view").on("click", function (e) {
+        console.log(e);
 
-            console.log(response);
-            // storing the data from the AJAX request in the results variable
-            var results = response.data;
 
-            // Looping through each result item
-            for (var i = 0; i < results.length; i++) {
+        // Grabbing and storing the data-animal property value from the button
+        var sports = $(e.target).attr("data-name");
+        console.log(e.target);
+        // Constructing a queryURL using the animal name
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            sports + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-                // Creating and storing a div tag
-                var sportsDiv = $("<div>");
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            // After data comes back from the request
+            .then(function (response) {
+                console.log("YO!");
+                // storing the data from the AJAX request in the results variable
+                var results = response.data;
 
-                // Creating a paragraph tag with the result item's rating
-                var p = $("<p>").text("Rating: " + results[i].rating);
+                // Looping through each result item
+                for (var i = 0; i < results.length; i++) {
 
-                // Creating and storing an image tag
-                var sportsImage = $("<img>");
-                // Setting the src attribute of the image to a property pulled off the result item
-                sportsImage.attr("src", results[i].images.fixed_height.url);
+                    // Creating and storing a div tag
+                    var sportsDiv = $("<div>");
 
-                // Appending the paragraph and image tag to the animalDiv
-                sportsDiv.append(p);
-                sportsDiv.append(sportsImage);
+                    // Creating a paragraph tag with the result item's rating
+                    var p = $("<p>").text("Rating: " + results[i].rating);
 
-                $("#gifs-appear-here").prepend(sportsDiv);
+                    // Creating and storing an image tag
+                    var sportsImage = $("<img>");
+                    // Setting the src attribute of the image to a property pulled off the result item
+                    sportsImage.attr("src", results[i].images.fixed_height.url);
+                    sportsImage.attr("data-still", results[i].images.fixed_height_still.url);
+                    sportsImage.attr("data-animate", results[i].images.fixed_height.url);
+                    sportsImage.attr("data-state", "still");
+                    sportsImage.addClass("pickles");
 
-            }
-        });
-});
+
+                    // Appending the paragraph and image tag to the animalDiv
+                    sportsDiv.append(p);
+                    console.log("Wake Up, Wake Up, Wake Up");
+                    sportsDiv.append(sportsImage);
+                    console.log("It's the first of da Month!")
+
+
+
+
+
+                    $("#gifs-appear-here").prepend(sportsDiv);
+                }
+
+                $(".pickles").on("click", function()  {
+
+                    var state = $(this).attr("data-state");
+                    if (state === "still") {
+                        $(this).attr("src", $(this).attr("data-animate"));
+                        $(this).attr("data-state", "animate");
+                      } else {
+                        $(this).attr("src", $(this).attr("data-still"));
+                        $(this).attr("data-state", "still");
+                      }
+
+                })
+            });
+    });
+
+};
+
+
+$(document).on("click", "#giphy-view", displayGifs);
+
+
+///have to click button 2 times and prepends 20 gifs...I changed the limit to 5 and it displays 10 gifs but repeats 5 of them.  
+//unable to empty div when button is pressed for gifs
+
